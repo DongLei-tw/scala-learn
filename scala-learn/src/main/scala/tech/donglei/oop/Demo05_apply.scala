2820b1af622cb2a1e957a6480d2441e4520bbdc2
Lei Dong <lei.dong@rea-group.com>
2022-10-18 22:32:31 +0800

    companion object - apply

---
+++ scala-learn/src/main/scala/tech/donglei/oop/Demo05_apply.scala
@@ -0,0 +1,20 @@
+package tech.donglei.oop
+
+class Demo05_apply {
+
+  class Person(var name: String = "", var age:Int = 0) {}
+
+  object Person {
+    def apply(name:String, age: Int): Person = new Person(name, age)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val person = new Person("Sam", 23)
+    println(person.name, person.age)
+
+    // if don't new a class, the apply method will be called
+    val person2 = Person("Bob", 18)
+    println(person2.name, person2.age)
+  }
+
+}
