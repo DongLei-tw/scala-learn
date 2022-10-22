25c333daea898ded73e84691af3b39150465d6a2
Lei Dong <lei.dong@rea-group.com>
2022-10-22 12:44:21 +0800

    Trait: extends multiple trait

--- scala-learn/src/main/scala/tech/donglei/traitdemo/Demo01.scala
+++ scala-learn/src/main/scala/tech/donglei/traitdemo/Demo01.scala
@@ -11,9 +11,31 @@ object Demo01 {
     override def log(msg: String): Unit = println(msg)
   }
 
+
+  trait MessageSender {
+    def send(msg: String):Unit
+  }
+
+  trait MessageReceiver {
+    def receive(): Unit
+  }
+
+  class MessageWorker extends MessageSender with MessageReceiver {
+    override def send(msg: String): Unit = println(s"Sending message: $msg")
+
+    override def receive(): Unit = println("I've got your message")
+  }
+
   def main(args: Array[String]): Unit = {
     val logger = new ConsoleLogger
 
     logger.log("class extends single trait")
+
+    // ---
+
+    val worker = new MessageWorker
+    worker.send("Hello world")
+    worker.receive()
+
   }
 }
