c4c762e5fa48816db7af36c15311715b5b57f654
Lei Dong <lei.dong@rea-group.com>
2022-10-25 23:29:36 +0800

    IO: Serialization

---
+++ scala-learn/src/main/scala/tech/donglei/io/Demo01_serialize_deserialize.scala
@@ -0,0 +1,20 @@
+package tech.donglei.io
+
+import java.io.{FileOutputStream, ObjectOutputStream}
+
+object Demo01_serialize_deserialize {
+
+  val filename = "Demo01_serialize_deserialize"
+
+  // If you want to serialize or deserialize you must extends Serializable
+  class Person(var name: String, var age: Int) extends Serializable
+
+  def main(args: Array[String]): Unit = {
+
+    // Serialization
+    val p = new Person("Sam", 23)
+    val oos = new ObjectOutputStream(new FileOutputStream(s"./data/out/$filename.txt"))
+    oos.writeObject(p)
+    oos.close()
+  }
+}
